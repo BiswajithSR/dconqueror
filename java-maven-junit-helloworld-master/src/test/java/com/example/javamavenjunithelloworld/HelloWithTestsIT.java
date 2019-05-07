f9f4b8aa7fe116ced204db4b4cd737b974270b43
BiswajithSR <31614061+BiswajithSR@users.noreply.github.com>
2019-05-07 20:06:12 +0530

    Update HelloWithTestsIT.java
    remove comment to make changes

--- java-maven-junit-helloworld-master/src/test/java/com/example/javamavenjunithelloworld/HelloWithTestsIT.java
+++ java-maven-junit-helloworld-master/src/test/java/com/example/javamavenjunithelloworld/HelloWithTestsIT.java
@@ -9,12 +9,10 @@ import static org.junit.Assert.*;
 
 /**
  * Integration test for the HelloApp program.
- * <p/>
  * An integration test verifies the workings of a complete program, a module, or a set of dependant classes.
  * <p/>
  * This integration test uses system-rules, an extension for JUnit that lets you test System.out and System.exit()
  * etc.:
- * <p/>
  * http://www.stefan-birkner.de/system-rules
  */
 public class HelloWithTestsIT {
